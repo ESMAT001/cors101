e02a0c7f5ea5abb4d758cbbfe41e4437f70a7750
Esmatullah Niazi <56363653+ESMAT001@users.noreply.github.com>
2026-09-24 22:19:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -199,5 +199,5 @@ else {
 
     }).listen(config.port);
 
-    console.log("thingproxy.freeboard.io process started (PID " + process.pid + ")");
+    console.log("process started (PID " + process.pid + ")");
 }
